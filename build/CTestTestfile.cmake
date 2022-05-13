a28f27cd1e811425ee3a53d76107cc8e77bab61a
yosefgoren <yosefisraelgoren@gmail.com>
2022-05-14 02:13:36 +0300

    removed build from repo

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/pc/Desktop/S6/courses/236832-Mkbuzar-Seminar/LockFreeHashset
-# Build directory: C:/Users/pc/Desktop/S6/courses/236832-Mkbuzar-Seminar/LockFreeHashset/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
